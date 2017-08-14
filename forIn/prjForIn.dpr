8f0036a0ceda3a75200b25db84680097cd96e081
huidemar <huidemar@gmail.com>
2017-08-14 00:38:28 -0300

    casos de usos para o comando For in

---
+++ forIn/prjForIn.dpr
@@ -0,0 +1,14 @@
+program prjForIn;
+
+uses
+  Vcl.Forms,
+  uMain in 'uMain.pas' {FMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFMain, FMain);
+  Application.Run;
+end.
